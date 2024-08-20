1ee6caa296d0e47a4cb8ebe990e860bf81ca99b8
Tanishka <anjanasharma8448766174@gmail.com>
2024-08-20 18:50:00 +0530

    done

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const userRouter = express.Router();
 const UserModel = require("../models/user.model");
 const jwt = require("jsonwebtoken");
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 
 const dotenv=require("dotenv").config();
